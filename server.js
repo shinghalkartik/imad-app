b594fec4de390f25009be47eebff91911a14fb74
shinghalkartik <33263911+shinghalkartik@users.noreply.github.com>
2018-02-27 19:35:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,13 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
+var counter = 0;
+
+app.get('/counter' , function (req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
